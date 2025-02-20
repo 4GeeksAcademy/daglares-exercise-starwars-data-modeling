83e0d4d864871490d106d2eb7ee6c8734c910f88
DAGarciaLopez <dagarcialopez5@gmail.com>
2025-02-20 03:36:29 +0000

    SW data modeling 1.0

--- src/models.py
+++ src/models.py
@@ -6,25 +6,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    username: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable = False, unique = True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
 
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__= 'favorites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_ID: Mapped[str] = mapped_column(ForeignKey('user.id'), nullable=True)
+    user: Mapped["User"] = relationship()
+
+    planets_ID: Mapped[int] = mapped_column(ForeignKey('planets.id'), nullable=True)
+    planets: Mapped["Planets"]=relationship()
+
+    people_ID: Mapped[int] = mapped_column(ForeignKey('people.id'), nullable = True)
+    people: Mapped["People"]=relationship()
+
+class People(Base):
+    __tablename__ = 'people'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    name: Mapped[str] = mapped_column(nullable=False)
+    mass: Mapped[int] = mapped_column(nullable=False)
+    eye_color: Mapped[str] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=False)
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    gravity: Mapped[str] = mapped_column(nullable=False)
+    diameter: Mapped[str] = mapped_column(nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
 
     def to_dict(self):
         return {}
